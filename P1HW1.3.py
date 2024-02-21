f02026d767c9471ededb627444d70295cb6cebd0
Trumbleh4717 <160040796+Trumbleh4717@users.noreply.github.com>
2024-02-21 09:53:22 -0500

    Add files via upload

---
+++ P1HW1.3.py
@@ -0,0 +1,17 @@
+#Hailey
+#2/21/2024
+#P1HW1
+#Variables and integers
+print('Enter an integer')
+num1 = int(input( ))
+print("you entered", num1)
+square=num1 **2
+cubed=num1 **3
+print(num1,"squared is",square)
+print("and",num1, "cubed is",cubed)
+print('Enter another integer')
+num2 = int(input( ))
+print(num1,"+",num2,"is",num1+num2)
+print(num1,"x",num2,"is",num1*num2)
+
+
